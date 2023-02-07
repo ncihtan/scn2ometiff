daf974ce4d063fd3b0708624a107784634a3a767
Adam Taylor <adam.taylor@sagebase.org>
2023-02-07 20:14:14 +0000

    Create convert_scn.sh

---
+++ convert_scn.sh
@@ -0,0 +1,13 @@
+path=$1
+
+series_count=$(tiffcomment $path| grep -c '</image>')
+echo "$series_count series found in $path"
+echo "Series 0 will be skipped as presumed to be macro image"
+
+START=1
+END=$(($series_count-1))
+
+for (( series=$START; series<=$END; series++ ))
+do
+   source twostep.sh $path $series
+done
