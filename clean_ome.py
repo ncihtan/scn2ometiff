eefcba4a131c80830084dacf927a96b8a77e2594
Adam Taylor <adam.taylor@sagebase.org>
2023-02-07 20:14:53 +0000

    Create clean_ome.py

---
+++ clean_ome.py
@@ -0,0 +1,18 @@
+from ome_types import from_xml, to_xml
+import sys
+from xml.etree import ElementTree as ET
+
+
+# Read the XML from the first script argument
+path = sys.argv[1]
+ome = from_xml(path, parser = 'lxml')
+
+# remove structured annotations
+ome.structured_annotations = []
+
+# remove acquisition_date from first image
+ome.images[0].acquisition_date = None
+
+xml = to_xml(ome)
+
+print(xml)
